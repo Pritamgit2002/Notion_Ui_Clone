519a88cde54ab6b15e8edd5581037df4a68eb320
Pritam Gain <pritamjob2021@gmail.com>
2024-01-21 18:47:23 +0530

    build: api of db

--- src/app/(main)/(routes)/docs/page.tsx
+++ src/app/(main)/(routes)/docs/page.tsx
@@ -1,14 +1,28 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import Image from "next/image";
 import React from "react";
 import { FiPlusCircle } from "react-icons/fi";
+import { api } from "../../../../../convex/_generated/api";
+import { toast } from "sonner";
 
 type Props = {};
 
 const docs = (props: Props) => {
    const { user } = useUser();
+   const create = useMutation(api.documents.create);
+
+   const onCreate = () => {
+    const promise = create({title: "Untitled"});
+
+    toast.promise(promise,{
+      loading:"Creating a new note...",
+      success:"New note created.",
+      error:"Failed to generate!!!"
+    });
+   };
   return (
     <div className=" min-h-screen h-full flex flex-col items-center justify-center space-y-4 ">
       <Image
@@ -21,7 +35,7 @@ const docs = (props: Props) => {
       <h2 className="text-xl font-semibold">
         Welcome to {user?.firstName}&apos;s Jotion
       </h2>
-      <Button className=" flex items-center justify-center gap-2 text-xl p-6">
+      <Button className=" flex items-center justify-center gap-2 text-xl p-6" onClick={()=>onCreate()} >
         <FiPlusCircle />
         <span>Create a note</span>
       </Button>
--- src/app/(main)/_components/navigation.tsx
+++ src/app/(main)/_components/navigation.tsx
@@ -6,10 +6,13 @@ import { FaAngleDoubleLeft } from "react-icons/fa";
 import { useMediaQuery } from "usehooks-ts";
 import { GiHamburgerMenu } from "react-icons/gi";
 import UserItem from "./user-Item";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
+  const documents = useQuery(api.documents.get);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -109,7 +112,10 @@ export const Navigation = () => {
           <UserItem/>
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document._id} >{document.title}</p>
+
+          ))}
         </div>
         <div
           onMouseDown={handleMouseDown}
